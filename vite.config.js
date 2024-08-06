fb24962af9a6f19a69a23d2caed95228e833f912
222112038 <falanarofako29@gmail.com>
2024-08-06 22:35:07 +0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://backend-webgis-simoanginangin.vercel.app',
+      "/api": {
+        target: "https://backend-webgis-simoanginangin.vercel.app",
         changeOrigin: true,
         secure: false,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
     },
   },
 });
