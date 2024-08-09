b17a65af4ec8f42fdf039858fad74a8373522e98
222112038 <falanarofako29@gmail.com>
2024-08-09 10:09:47 +0700

    coba edit api config

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.API_URL,
+  baseURL: "https://backend-webgis-simoanginangin.vercel.app",
 });
 
 // Add a request interceptor
