a0abfacbe30473bac6777d531236012412d4f77e
222112038 <falanarofako29@gmail.com>
2024-09-02 14:45:30 +0700

    updateee

--- src/components/SimoketawangUsahaTable/data.js
+++ src/components/SimoketawangUsahaTable/data.js
@@ -25,7 +25,7 @@ const pemanfaatan_produk = [
   { key: "batik_ecoprint", label: "Batik ecoprint" },
   { key: "minuman", label: "Minuman klengkeng" },
   { key: "makanan", label: "Makanan klengkeng" },
-  { key: "tidak-dimanfaatkan", label: "Tidak dimanfaatkan" },
+  { key: "tidak_dimanfaatkan", label: "Tidak dimanfaatkan" },
 ];
 
 export {
