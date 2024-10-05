7f84846b6c06a00484e7ef746b1fbee6c5d8598b
222112038 <falanarofako29@gmail.com>
2024-10-05 10:45:16 +0700

    update error

--- src/components/BuletinAdm/buletinAdm.jsx
+++ src/components/BuletinAdm/buletinAdm.jsx
@@ -334,17 +334,17 @@ const BuletinAdm = () => {
           onError(error);
         });
     },    
-    // onChange(info) {
-    //   const { status } = info.file;
-    //   if (status !== "uploading") {
-    //     console.log(info.file, info.fileList);
-    //   }
-    //   if (status === "done") {
-    //     message.success(`${info.file.name} file uploaded successfully.`);
-    //   } else if (status === "error") {
-    //     message.error(`${info.file.name} file upload failed.`);
-    //   }
-    // },
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        // message.error(`${info.file.name} file upload failed.`);
+      }
+    },
     onDrop(e) {
       console.log("Dropped files", e.dataTransfer.files);
     },
